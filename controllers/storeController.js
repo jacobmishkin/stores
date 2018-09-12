b8577aa23e1fd3213665fce7428ff3bf31d74c74
Jacob Mishkin <user@MacBook-Pro-3.local>
2018-09-12 07:49:58 -0500

    added: geolocation indexes

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -116,3 +116,21 @@ exports.searchStores = async (req, res) => {
     .limit(5);
   res.json(store);
 };
+
+exports.mapStores = async (req, res) => {
+  const coordinates = [req.query.lng, req.query.lat].map(parseFloat);
+  const q = {
+    location: {
+      $near: {
+        $geometry: {
+          type: 'Point',
+          coordinates
+        },
+        $maxDistance: 10000
+      }
+    }
+  };
+
+  const stores = await Store.find(q)
+  res.json(stores);
+}
